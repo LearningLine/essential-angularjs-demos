62336e0578155d536de6deff470a3e57637c5fbc
Maurice de Beijer <mauricedb@computer.org>
2015-09-17 13:49:23 +0100

    and

---
+++ Mod04/gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+var mainBowerFiles = require('gulp-main-bower-files');
+
+gulp.task('mainBowerFiles', function () {
+    return gulp.src('./bower.json')
+        .pipe(mainBowerFiles({
+            overrides: {
+                bootstrap: {
+                    main: [
+                        './dist/js/bootstrap*.js',
+                        './dist/css/*.*',
+                        './dist/fonts/*.*'
+                    ]
+                }
+            }
+        }))
+        .pipe(gulp.dest('./public/libs/'))
+});
+
+gulp.task('default', ['mainBowerFiles']);
---
+++ Mod04/public/app/app.js
@@ -0,0 +1,52 @@
+/**
+ * Created by maurice on 9/17/2015.
+ */
+
+(function () {
+'use strict';
+    var theApp = angular.module('theApp', ['ngResource']);
+
+    theApp.controller('DemoController', function ($http) {
+        var _this = this;
+
+        $http.get('/api/books').then(function (e) {
+            //console.log(this);
+            _this.books = e.data;
+
+        }, function (err) {
+            console.log(err);
+        });
+
+        this.addBook = function(){
+            $http.post('/api/books', {title:"The new book"});
+        }
+    });
+
+    theApp.factory('Book', function($resource){
+        return $resource('/api/books/:id');
+    });
+
+    theApp.controller('ResourceController', function (Book) {
+            this.books = Book.query();
+console.log(this.books.length);
+
+        this.addBook = function(){
+            //$http.post('/api/books', {title:"The new book"});
+        }
+    });
+
+
+    //function query(){
+    //    var books = [];
+    //
+    //    $http.get('...').then(function(e){
+    //        e.data.forEach(function(book){
+    //            book.push(book);
+    //        })
+    //    })
+    //
+    //    return books;
+    //}
+    
+
+}());
